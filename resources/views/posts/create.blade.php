c899ca854b24cf2527366338daf2eb0018719360
watahaizian <watahaizian@gmail.com>
2023-11-13 20:54:54 +0900

    create-view

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Posts</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body>
+        <h1>Blog Name</h1>
+        <form action="/posts" method="POST">
+            @csrf
+            <div class="title">
+                <h2>タイトル</h2>
+                <input type="text" name="post[title]" placeholder="タイトル"/>
+            </div>
+            <div class="body">
+                <h2>内容</h2>
+                <textarea name="post[body]" placeholder="今日も1日お疲れさまでした。"></textarea>
+            </div>
+            <input type="submit" value="保存"/>
+        </form>
+        <div class="footer">
+            <a href="/">戻る</a>
+        </div>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,5 @@ use App\Http\Controllers\PostController;
 
 Route::get('/', [PostController::class, 'index']);
 Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/create', [PostController::class, 'create']);
 Route::get('/posts/{post}', [PostController::class, 'show']);
