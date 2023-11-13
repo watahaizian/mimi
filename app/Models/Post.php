195fd82cda605183f7b7b13436587c6ed0fdd8da
watahaizian <watahaizian@gmail.com>
2023-11-13 21:29:49 +0900

    create-store

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    protected $fillable = [
+        'title',
+        'body',
+    ];
     use HasFactory;
     
     public function getByLimit(int $limit_count = 10)
--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,6 @@ use App\Http\Controllers\PostController;
 
 Route::get('/', [PostController::class, 'index']);
 Route::get('/posts', [PostController::class, 'index']);
+Route::post('/posts', [PostController::class, 'store']);
 Route::get('/posts/create', [PostController::class, 'create']);
 Route::get('/posts/{post}', [PostController::class, 'show']);
