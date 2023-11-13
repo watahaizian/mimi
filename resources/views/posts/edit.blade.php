db7694ab0960613f3bd3185ebe97038a108e203c
watahaizian <watahaizian@gmail.com>
2023-11-13 23:36:30 +0900

    edit

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -9,19 +9,20 @@
     </head>
     <body>
         <h1>Blog Name</h1>
-        <form action="/posts" method="POST">
+        <form action="/posts/{{ $post->id }}" method="POST">
             @csrf
-            <div class="title">
+            @method('PUT')
+            <div class='content__title'>
                 <h2>タイトル</h2>
-                <input type="text" name="post[title]" placeholder="タイトル" value="{{ $post->title }}"/>
+                <input type='text' name='post[title]' value="{{ $post->title }}">
                 <p class="title__error" style="color:red">{{ $errors->first('post.title') }}</p>
             </div>
-            <div class="body">
-                <h2>内容</h2>
-                <textarea name="post[body]" placeholder="今日も1日お疲れさまでした。">{{ $post->body }}</textarea>
+            <div class='content__body'>
+                <h2>本文</h2>
+                <input type='text' name='post[body]' value="{{ $post->body }}">
                 <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
             </div>
-            <input type="submit" value="保存"/>
+            <input type="submit" value="保存">
         </form>
         <div class="footer">
             <a href="/">戻る</a>
